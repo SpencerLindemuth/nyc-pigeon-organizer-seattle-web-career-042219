7605c79364cd6b729cba0f980575e671ac70a47a
Spencer Lindemuth <lindemut@gmail.com>
2019-04-05 17:12:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
-  data.each_pair do |key, value|
-      value.each_pair do |attribute, names|
+  data.each do |key, value|
+      value.each do |attribute, names|
         names.each do |name|
           binding.pry
           if new_hash[name].nil?
