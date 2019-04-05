29a0a1391b3571e5ade7752c9088b34520875d38
Spencer Lindemuth <lindemut@gmail.com>
2019-04-05 16:55:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,10 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   data.each_pair do |key, value|
-      value.each do |attribute, name|
-        binding.pry
+      value.each do |attribute, names|
+        name.each do |name|
+          binding.pry
+        end
       end
   end
 end
