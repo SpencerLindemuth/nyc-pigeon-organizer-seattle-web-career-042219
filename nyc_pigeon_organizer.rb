3ecf56b53a59d79cabce02bcf28472b75bc4a7a1
Spencer Lindemuth <lindemut@gmail.com>
2019-04-05 16:47:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,9 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(k, h) , stats|
-    binding.pry
+    h.each do |x, y|
+      binding.pry
+    end
 
   end
 end
