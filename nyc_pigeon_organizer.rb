8bef8b6fa021a5812d6bb132c4a32d1bc5761aee
Spencer Lindemuth <lindemut@gmail.com>
2019-04-05 16:43:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,12 +2,10 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-  new_hash = {}
-  data.each do |category, stats|
+  data.each_with_object({}) do |category, stats|
     stats.each do |info, names|
       names.each do |name|
-        binding.pry
-          (new_hash[name][category] ||= []) << info.to_s
+
         end
     end
     end
