e7e94e6b4409c6513e00c4c13edfea346badf41f
Spencer Lindemuth <lindemut@gmail.com>
2019-04-05 16:59:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,6 +9,7 @@ def nyc_pigeon_organizer(data)
           binding.pry
           if new_hash[name].nil?
             new_hash[name] = {}
+            new_hash[name][key] = []
           end
         end
       end
